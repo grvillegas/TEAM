2d3eda348f2bba40f2bde466d4a161da51656b75
Gustavo Rodriguez <gustavo.rodriguezv30@gmail.com>
2021-12-11 14:57:57 -0400

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace team
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int salida=0;
+            
+           
+            n = Int32.Parse(Console.ReadLine());
+            
+            for (int i =0;i<n;i++) {
+                  int[] a = (from string s in Console.ReadLine().Split(' ') select Convert.ToInt32(s)).ToArray();
+                
+                  int contador =0;
+
+                  foreach (int votacion in a) {
+                      if (votacion ==1) {
+                          contador ++;
+                      }
+                  }
+                  if (contador >=2) {
+                      salida++;
+                  }
+            }
+            Console.WriteLine(salida);
+        }
+    }
+}
